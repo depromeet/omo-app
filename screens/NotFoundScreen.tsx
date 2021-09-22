978362e1e162ee0982372a0ee17ba129c4884d96
박종호 <jong951005@gmail.com>
2021-09-22 16:29:48 +0900

    linting 옵션들 추가

--- screens/NotFoundScreen.tsx
+++ screens/NotFoundScreen.tsx
@@ -3,7 +3,9 @@ import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 import { RootStackScreenProps } from '../types';
 
-export default function NotFoundScreen({ navigation }: RootStackScreenProps<'NotFound'>) {
+export default function NotFoundScreen({
+  navigation,
+}: RootStackScreenProps<'NotFound'>): JSX.Element {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>This screen doesn't exist.</Text>
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,31 +1,28 @@
-import { useCallback, useRef, useState } from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 import { BackHandler } from 'react-native';
 import WebView from 'react-native-webview';
 
 import { useFocusEffect } from '@react-navigation/native';
 
-import { RootTabScreenProps } from '../types';
-
 const uri = 'https://www.inflearn.com/';
 
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+export default function TabOneScreen(): JSX.Element {
   const webview = useRef<WebView>(null);
   const [canGoBack, setCanGoBack] = useState(false);
 
-  const onAndroidBackPress = () => {
-    if (webview.current && canGoBack) {
-      console.log(`cangoBack : ${canGoBack}`);
-      webview.current.goBack();
-      return true;
-    }
-    //FIXME: 일단 Android 환경에서 canGoBack이 false일 경우 exitApp을 하도록 했는데,
-    // 추후에 문제가 발생할 수 있으니 염두해 둘것
-    BackHandler.exitApp();
-    return false;
-  };
-
   useFocusEffect(
     useCallback(() => {
+      const onAndroidBackPress = () => {
+        if (webview.current && canGoBack) {
+          console.log(`cangoBack : ${canGoBack}`);
+          webview.current.goBack();
+          return true;
+        }
+        //FIXME: 일단 Android 환경에서 canGoBack이 false일 경우 exitApp을 하도록 했는데,
+        // 추후에 문제가 발생할 수 있으니 염두해 둘것
+        BackHandler.exitApp();
+        return false;
+      };
       BackHandler.addEventListener('hardwareBackPress', onAndroidBackPress);
       return () => {
         BackHandler.addEventListener('hardwareBackPress', onAndroidBackPress);
